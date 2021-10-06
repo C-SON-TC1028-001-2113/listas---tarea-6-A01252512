c3926c73dd6a658c1c38ccd674aa14fa5763682a
A01252512 <a01252512@itesm.mx>
2021-10-06 16:11:53 +0000

    V1

--- assignments/09SerieFibonacciListas/src/exercise.py
+++ assignments/09SerieFibonacciListas/src/exercise.py
@@ -1,6 +1,22 @@
 def main():
+    fibonacci = []
+    n = int(input())
+    while n < 0:
+        n = int(input())
     
-    pass
+    if n == 0:
+        print(fibonacci)
+    
+    elif n == 1:
+        fibonacci = [0]
+        print(fibonacci)
+    
+    else:
+        fibonacci = [0,1]
+        for i in range(n-2):
+            num_siguiente = fibonacci[i] + fibonacci[i+1]
+            fibonacci.append(num_siguiente)
+        print(fibonacci)    
 
 if __name__=='__main__':
     main()
